02826f29244afa43f2eeac8983caa4c03944fcd9
holley <houli@babyeye.com>
2020-07-06 11:33:46 +0800

    使用创建对象的形式创建字符串变量，也不能修改

--- src/test/java/com/zhl/practice/ForeachTest.java
+++ src/test/java/com/zhl/practice/ForeachTest.java
@@ -8,15 +8,17 @@ import java.util.List;
 
 /**
  * @author Holley
- * @description 请输入一句话进行描述
+ * @description 测试foreach遍历时，是否能够改变数据
+ * 结论：基本数据类型和字符串类型不能改变，对象的可以改变属性值
  * @create 2020-07-06 10:49
  **/
 public class ForeachTest {
 
     public static void main(String[] args) {
         List<String> stringList = Lists.newArrayList();
-        stringList.add("测试");
-        stringList.add("成功");
+//        stringList.add("测试");
+//        stringList.add("成功");
+        stringList.add(new String("原数据"));
         for(String str:stringList){
             str = "修改数据";
             System.out.println(str);
@@ -26,12 +28,17 @@ public class ForeachTest {
         }
         List<Person> oldList = new ArrayList<>();
         Person p = new Person("1","1");
-        Person p2 = new Person("2","2");
         oldList.add(p);
-        oldList.add(p2);
         for(Person person:oldList) {
             System.out.println(person.getName());
             person.setName("测试");
+        }
+        for(Person person:oldList) {
+            System.out.println(person.getName());
+        }
+        for(Person person:oldList) {
+            person = new Person("2","2");
+            System.out.println(person.getName());
         }
         for(Person person:oldList) {
             System.out.println(person.getName());
